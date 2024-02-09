0ab07a39462a9fd529e9e89c92ebe05587bdd4ba
Hadas Cohen <hc820@live.mdx.ac.uk>
2024-02-09 09:57:33 +0000

    inital commit - updated code

--- sumrange.cpp
+++ sumrange.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-int main(){
 
+int main() {
+    // Declare variables to store user input
+    int firstNumber, secondNumber;
 
+    // Get input from the user
+    std::cout << "Enter the first number: ";
+    std::cin >> firstNumber;
 
+    std::cout << "Enter the second number: ";
+    std::cin >> secondNumber;
 
+    // Calculate and display the sum
+    int sum = 0;
+    for (int i = firstNumber; i <= secondNumber; ++i) {
+        sum += i;
+    }
+
+    std::cout << "The sum from " << firstNumber << " to " << secondNumber << " is: " << sum << std::endl;
+
+    return 0;
 }
+
